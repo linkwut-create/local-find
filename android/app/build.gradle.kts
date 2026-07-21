e1c3b1842dd062860557f739b350290011280dff
林深莫闻鹿 <linkwut@gmail.com>
2026-07-22 06:46:50 +0800

    build: target Android 16 API 36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,14 +29,14 @@ plugins {
 
 android {
     namespace = "io.github.linkwutcreate.localfind"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "io.github.linkwutcreate.localfind"
         minSdk = 26
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        targetSdk = 36
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
